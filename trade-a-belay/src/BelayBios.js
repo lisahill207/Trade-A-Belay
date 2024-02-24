84488b8a214ea0d59e6ac82a9896b3df6dd35182
Lisa Hill <lisahill207@gmail.com>
2024-02-24 12:02:09 -0500

    css border dots

--- trade-a-belay/src/BelayBios.js
+++ trade-a-belay/src/BelayBios.js
@@ -4,11 +4,15 @@ import StaticBios from "./StaticBios";
 export default function BelayBios({ deleteBio, bios }) {
   return (
     <div className="bios">
-      <StaticBios />
-      {bios.length === 0 && ""}
-      {bios.map((bio) => {
-        return <NewBio {...bio} key={bio.id} deleteBio={deleteBio} />;
-      })}
+      <div className="outer-dots">
+        <div className="inner-dots">
+          <StaticBios />
+          {bios.length === 0 && ""}
+          {bios.map((bio) => {
+            return <NewBio {...bio} key={bio.id} deleteBio={deleteBio} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
