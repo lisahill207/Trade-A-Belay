d37a8f562210aa4bdbf0166a8ebbe080e8e08421
Lisa Hill <lisahill207@gmail.com>
2024-03-05 13:49:12 -0500

    add title

--- trade-a-belay/src/BelayBios.js
+++ trade-a-belay/src/BelayBios.js
@@ -6,11 +6,17 @@ export default function BelayBios({ deleteBio, bios }) {
     <div className="bios">
       <div className="outer-dots">
         <div className="inner-dots">
-          <StaticBios />
-          {bios.length === 0 && ""}
-          {bios.map((bio) => {
-            return <NewBio {...bio} key={bio.id} deleteBio={deleteBio} />;
-          })}
+          <div className="upper">
+            <h2>Trade-A-Belay</h2>
+            <p>Where climbing partnerships begin</p>
+          </div>
+          <div className="lower">
+            <StaticBios />
+            {bios.length === 0 && ""}
+            {bios.map((bio) => {
+              return <NewBio {...bio} key={bio.id} deleteBio={deleteBio} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
