1f90e618baa8571b0c5947f1ea9d6241b2bde32c
Lisa Hill <lisahill207@gmail.com>
2024-09-05 15:39:55 -0400

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
